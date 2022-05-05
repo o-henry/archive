57f79c7de8672c7098fd61fb9424cd2bd78c76ca
henry <cksgod16@gmail.com>
2022-05-05 14:07:46 +0900

    init

---
+++ src/main/java/com/henry/archive/ArchiveApplication.java
@@ -0,0 +1,13 @@
+package com.henry.archive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/henry/archive/ArchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henry.archive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
