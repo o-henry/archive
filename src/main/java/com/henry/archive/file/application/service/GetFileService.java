c0ed2c4affccf4cb1f0e418a76d043f803dc000c
henry <cksgod16@gmail.com>
2022-05-08 11:09:58 +0900

    fix: line 9 List -> Optional

--- src/main/java/com/henry/archive/file/application/service/GetFileService.java
+++ src/main/java/com/henry/archive/file/application/service/GetFileService.java
@@ -3,11 +3,12 @@ package com.henry.archive.file.application.service;
 import com.henry.archive.file.application.port.in.query.GetFileQuery;
 import com.henry.archive.file.domain.File;
 import java.util.List;
+import java.util.Optional;
 
 public class GetFileService implements GetFileQuery {
 
   @Override
-  public List<File> findByHidden(Boolean hidden) {
+  public Optional<File> findByHidden(Boolean hidden) {
     return null;
   }
 
